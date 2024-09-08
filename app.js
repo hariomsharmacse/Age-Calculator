347e9de04b8b46dfe6b59bcbe1715d220c99693c
hariomsharmacse <hariomsharmacse@gmail.com>
2024-09-08 09:43:43 +0530

    add

--- app.js
+++ app.js
@@ -1,29 +1,55 @@
-const inputDate = document.getElementById('date');
-const calBtn = document.getElementById('btn');
-const output = document.getElementById('output');
+const inputDate = document.getElementById("date");
+inputDate.max = new Date().toISOString().split("T")[0];
+const calBtn = document.getElementById("btn");
+const output = document.getElementById("output");
 
+calBtn.addEventListener("click", () => {
+  if (inputDate.value === "") {
+    alert("Please enter a valid value");
+  } else {
+    let birthDate = new Date(inputDate.value);
 
-calBtn.addEventListener('click', () => {
-    let dob = inputDate.value;
-    let dateOne = new Date(dob);
-    let currDate = new Date();
+    let d1 = birthDate.getDate();
+    let m1 = birthDate.getMonth() + 1;
+    let y1 = birthDate.getFullYear();
 
-    // let month = currDate.getMonth() - dateOne.getMonth();
+    let today = new Date();
 
-    // let day = currDate.getDay() - dateOne.getDay();
+    let d2 = today.getDate();
+    let m2 = today.getMonth() + 1;
+    let y2 = today.getFullYear();
 
-    // let year = currDate.getFullYear() - dateOne.getFullYear();
+    console.log(d2);
 
-    let differece = currDate - dateOne;
+    let d3, m3, y3;
 
-    const finalYears = Math.floor((differece/(24 * 60 * 60 * 1000))/365.25);
+    y3 = y2 - y1;
 
+    if (m2 >= m1) {
+      m3 = m2 - m1;
+    } else {
+      y3--;
+      m3 = 12 + m2 - m1;
+    }
+
+    if (d2 >= d1) {
+      d3 = d2 - d1;
+    } else {
+      m3--;
+      d3 = getDaysInMonth(y1, m1) + d2 - d1;
+    }
+
+    if (m3 < 0) {
+      m3 = 11;
+      y3--;
+    }
+
+    console.log(d3, m3, y3);
     output.style.display = "block";
-    output.innerText = `Your are ${finalYears} years old`
+    output.innerText = `Your age is ${y3}years ${m3}months and ${d3}days.`;
+  }
+});
 
-
-})
-
-
-
-
+function getDaysInMonth(year, month) {
+  return new Date(year, month, 0).getDate();
+}
